da2fcf704330be7f2c4c487eb38da6a332f3e2a5
Dani Matuko <danimatuko@pop-os.lan>
2024-10-30 15:45:10 +0200

    Special content styles & svgr plugin
    - Added mobile style for the special content section.
    - Imported svgr plugin to help loading svg as a React component.
    
    The SVGR plugin is required due to the implementation of components in the project.
    q

--- src/components/ContentSectionSpecial.jsx
+++ src/components/ContentSectionSpecial.jsx
@@ -5,16 +5,21 @@ import { useHeroContent } from '../hooks/useHeroContent';
 import { useIconPath } from '../hooks/useIconPath';
 import Arrow from './svg/Arrow';
 import Button from './Button';
+import SectionIcon from "../assets/icons/icon-4.svg?react";
+
 
 function ContentSectionSpecial({ dataName }) {
   const { data: contentData, error: contentError } = useContentData(dataName);
   const { heroContent, error: heroError } = useHeroContent('heroData');
-
   const item = heroContent?.items.find(item => item.name === dataName);
+
+  // Check if item exists before destructuring
+  const { additionalLinks, linkLabel } = item || {};
+
   const iconPath = useIconPath(contentData?.heroLink?.icon);
 
   if (contentError || heroError) {
-    return;
+    return <p>Error loading content</p>;
   }
   if (!contentData || !heroContent) {
     return <p>Loading...</p>;
@@ -29,12 +34,14 @@ function ContentSectionSpecial({ dataName }) {
       style={{ backgroundImage: `url(https://connecteam.com/static/frontend-home-task/jpg/${dataName}-small.jpg)` }}
     >
       {/* Overlay for the transparent background color */}
-      <div className='absolute inset-0 bg-black opacity-30' /> {/* Adjust opacity as needed */}
+      <div className='absolute inset-0 opacity-50' style={{ backgroundColor: fillColor }} /> {/* Increased opacity for better coverage */}
 
       <div className='container mx-auto p-5 relative z-10'>
         <div className="flex items-center gap-3 mb-4">
           <div className="flex flex-col">
-            <img className="w-16 mb-2" src={iconPath} alt="" />
+            {/* Use as React Component */}
+            <SectionIcon className="w-16 h-16 mb-2" fill={fillColor} />
+            {/* Use as Image Source */}
             <p className="text-sm text-white">{contentData.label}</p>
             <h2
               className="text-4xl font-bold mb-4"
@@ -48,11 +55,25 @@ function ContentSectionSpecial({ dataName }) {
           className="description text-white font-medium text-lg"
           dangerouslySetInnerHTML={{ __html: contentData.description }}
         />
-        <hr className='my-5 border-gray-500' />
-        <div className='flex flex-col gap-4'>
-          <Button text="Click Me" color={fillColor} variant="filled" icon={<Arrow />} iconPosition="right" />
-          <Button text="Click Me" color={fillColor} variant="outlined" icon={<Arrow color={fillColor} />} iconPosition="left" />
-          <Button text="Click Me" color={fillColor} variant="outlined" icon={<Arrow color={fillColor} />} iconPosition="left" />
+        <div className='my-5 w-full h-0.5' style={{ backgroundColor: "rgba(255, 255, 255, 0.08)" }}></div>
+        <div className='flex flex-col gap-4 py-5'>
+          <Button
+            text={linkLabel}
+            color={fillColor}
+            variant="filled"
+            icon={<Arrow />}
+            iconPosition="right"
+          />
+          {additionalLinks?.map((link, index) => (
+            <Button
+              key={index}
+              text={link.label}
+              color={fillColor}
+              variant="outlined"
+              icon={<Arrow color={fillColor} />}
+              iconPosition="left"
+            />
+          ))}
         </div>
       </div>
     </section>
