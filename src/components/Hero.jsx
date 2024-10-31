35275e036e42f048abdf83e9c41f0466f2f39363
Dani Matuko <danimatuko@pop-os.lan>
2024-10-31 20:43:02 +0200

    change icon sources

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,3 @@
-
 // Import React and necessary assets
 import React from 'react';
 import heroData from '../data/heroData.json';
@@ -9,12 +8,12 @@ import Button from './Button';
 import Arrow from './svg/Arrow';
 import Icon1 from "../assets/icons/icon-1.svg?react";
 
-
 function Hero() {
   const { title, subtitle, links } = heroData.content.hero;
 
   return (
     <section className="hero-section relative text-white flex flex-col justify-center items-center px-10">
+      <Header />
 
       <div className="flex flex-col md:w-3/4 lg:w-1/3 self-start lg:ml-[20%] mt-60 lg:-mt-52">
 
@@ -38,14 +37,17 @@ function Hero() {
           />
         </div>
 
-        <img
-          src={arrowScroll}
-          alt="Scroll down arrow"
-          className="mx-auto mt-10 lg:absolute bottom-12 animate-bounce w-10"
-        />
+        <a href="#repudiandae">
+          <img
+            src={arrowScroll}
+            alt="Scroll down arrow"
+            className="mx-auto mt-10 lg:absolute bottom-12 animate-bounce w-10 hover:animate-none"
+          />
+        </a>
       </div>
     </section>
   );
 }
 
 export default Hero;
+
